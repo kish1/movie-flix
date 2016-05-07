30b57557a9b31979d3824418b3b96f6a5d9a06ca
Kishore Sridhar <kish.connect@gmail.com>
2016-05-07 19:50:02 -0400

    feat(module-ui): design homepage and set up event handlers for sign in and sign up

--- client/views/home/home.controller.js
+++ client/views/home/home.controller.js
@@ -6,6 +6,29 @@
         .module("MovieFlixApp")
         .controller("HomeController", HomeController);
     function HomeController() {
+        var vm = this;
+
+        vm.loginError = null;
+        vm.registerError = null;
+
+        vm.login = {};
+        vm.register = {};
+
+        vm.loginUser = loginUser;
+        vm.registerUser = registerUser;
+
+        var init = function () {
+
+        };
+        init();
+
+        function loginUser(user) {
+
+        }
+
+        function registerUser(user) {
+
+        }
 
     }
 })();
