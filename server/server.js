5a8f88041239ee5c5349938b007fbbbed3ed1c25
Kishore Sridhar <kish.connect@gmail.com>
2016-05-12 17:42:19 -0400

    feat(module-api): include and initialize packages used by express.js and mongoDB

--- server/server.js
+++ server/server.js
@@ -2,12 +2,31 @@
  * Created by kishore on 4/25/16.
  */
 var express = require("express");
-
 var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var mongoose = require('mongoose');
+
+var connectionString = 'mongodb://127.0.0.1:27017/movieflix';
+var db = mongoose.connect(connectionString);
 
 app.use(express.static(__dirname + "/client"));
 
 var ipaddress = "127.0.0.1";
 var port = 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+app.use(cookieParser());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+
+require("./app.js")(app, mongoose);
 
 app.listen(port, ipaddress);
