b171541042df4e4d2d4707cfe51b7e583765b77e
Kishore Sridhar <kish.connect@gmail.com>
2016-05-14 01:42:24 -0400

    feat(module-api): complete admin service

--- server/services/admin.service.server.js
+++ server/services/admin.service.server.js
@@ -1,3 +1,67 @@
 /**
  * Created by kishore on 5/12/16.
  */
+"use strict";
+
+module.exports = function (app, titleModel) {
+    app.post("/api/admin/title", addTitle);
+    app.put("/api/admin/title/:titleId", editTitle);
+    app.delete("/api/admin/title/:titleId", deleteTitle);
+
+    function deleteTitle(req, res) {
+        if (isAdmin(req.user)) {
+            var titleId = req.params.titleId;
+            titleModel
+                .deleteTitle(titleId)
+                .then(function (response) {
+                        res.json(response);
+                    },
+                    function (err) {
+                        res.status(400).send(err);
+                    });
+        } else {
+            res.status(403);
+        }
+
+    }
+
+    function editTitle(req, res) {
+        if (isAdmin(req.user)) {
+            var titleId = req.params.titleId;
+            var newTitle = req.body;
+            titleModel
+                .editTitle(titleId, newTitle)
+                .then(function (title) {
+                        res.json(title);
+                    },
+                    function (err) {
+                        res.status(400).send(err);
+                    });
+        } else {
+            res.status(403);
+        }
+    }
+
+    function addTitle(req, res) {
+        if(isAdmin(req.user)) {
+            var title = req.body;
+            titleModel
+                .addTitle(title)
+                .then(function (response) {
+                        res.json(response);
+                    },
+                    function (err) {
+                        res.status(400).send(err);
+                    });
+        } else {
+            res.status(403);
+        }
+    }
+
+    function isAdmin(user) {
+        if(user.name === "Admin") {
+            return true
+        }
+        return false;
+    }
+};
